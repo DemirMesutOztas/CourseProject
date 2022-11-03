921b489736dc0f83d56a9981b739c122ba8b505f
Demir Mesut ÖZTAŞ <48533725+DemirMesutOztas@users.noreply.github.com>
2022-11-03 11:50:54 +0300

    third

--- CourseProject/trainee.aspx.cs
+++ CourseProject/trainee.aspx.cs
@@ -12,21 +12,13 @@ namespace CourseProject
 {
     public partial class trainee : System.Web.UI.Page
     {
-        //SqlConnection baglanti = new SqlConnection(WebConfigurationManager.ConnectionStrings["veritabani"].ConnectionString);
-        //SqlConnection baglanti2 = new SqlConnection("Data Source=dmo.database.windows.net; Initial Catalog=trainee;Integrated Security=True");
+        SqlConnection baglanti = new SqlConnection(WebConfigurationManager.ConnectionStrings["veritabani"].ConnectionString);
+       
 
 
         protected void Page_Load(object sender, EventArgs e)
         {
 
-
-            string connetionString;
-            SqlConnection cnn;
-            connetionString = ConfigurationManager.ConnectionStrings["veritabani"].ConnectionString;
-            cnn = new SqlConnection(connetionString);
-            cnn.Open();
-            Response.Write("Connection Made");
-            cnn.Close();
 
 
 
@@ -34,11 +26,13 @@ namespace CourseProject
 
         protected void user_kaydet_Click(object sender, EventArgs e)
         {
-            //baglanti2.Open();
-            ////SqlCommand gonder = new SqlCommand("insert into trainee (name,surname,email,phone,age,education, password, password2 ) values('" + t_txt_adi.Text + "','" + t_txt_surname.Text + "','" + t_txt_email.Text + "','" + t_txt_phone.Text + "','" + t_txt_age.Text + "','" + t_txt_education.Text + "','" + t_txt_password.Text + "','" + t_txt_password2.Text +"')'", baglanti2);
-            //gonder.ExecuteNonQuery();
-            //baglanti2.Dispose();/*Nesneyi bellekten at*/
-            //baglanti2.Close();
+            SqlConnection baglanti = new SqlConnection("Data Source= .; Initial Catalog=dbo.table;Integrated Security=True");
+            baglanti.Open();
+            // SqlCommand gonder = new SqlCommand("insert into trainee (name,surname,email,phone,age,education, password, password2 ) values('" + t_txt_adi.Text + "','" + t_txt_surname.Text + "','" + t_txt_email.Text + "','" + t_txt_phone.Text + "','" + t_txt_age.Text + "','" + t_txt_education.Text + "','" + t_txt_password.Text + "','" + t_txt_password2.Text +"')'", baglanti);
+             SqlCommand gonder = new SqlCommand("insert into dbo.dmo (adi,soyadi) values('" + t_txt_adi.Text + "','" + t_txt_surname.Text  +"')'", baglanti);
+            gonder.ExecuteNonQuery();
+            baglanti.Dispose();/*Nesneyi bellekten at*/
+            baglanti.Close();
 
         }
     }
