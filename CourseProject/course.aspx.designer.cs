f7942db40ab024373c4578b1f4002b3a1eaae58c
Gogle Öztaş <demiroztas06@hotmail.com>
2022-11-09 10:39:56 +0300

    okul

--- CourseProject/course.aspx.designer.cs
+++ CourseProject/course.aspx.designer.cs
@@ -40,5 +40,14 @@ namespace CourseProject
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+
+        /// <summary>
+        /// Calendar1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Calendar Calendar1;
     }
 }
